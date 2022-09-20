e26c0e40317ef1561a1da03583813889acfb25bb
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-09-20 13:09:53 +0200

    ejercicio 2 de la clase hecho

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,2 @@
-alert("hello!");
-
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+let btnCreate = document.querySelector (".create");
+console.log(btnCreate);
