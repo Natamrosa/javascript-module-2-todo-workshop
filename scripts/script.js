b84a008177de450a2295a5aa2c1da7d5286b3ac4
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-09-20 19:34:06 +0200

    ejercicio 10 hecho

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,7 @@ console.log(btnCreate);*/
 
 5//
 
-let todos =[]
+/*let todos =[]
 
 const  createTodo = (text) => {
     todos.push(text)
@@ -35,6 +35,75 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
 
     console.log(todos);
 })
+*/
+
+
+10//
+
+let todos = []
+
+const removeTodo = (todoEl) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+    })
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+    
+const renderTodos = (todos) => {
+    const todoList = document.querySelector("#todos")
+    todoList.innerHTML =  " "
+
+    if (todos.length > 0) {
+        todos.forEach((todo) => {todoList.appendChild(generateTodoDOM(todo))})
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+
+}
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    todoText.textContent = todo
+    containerEl.appendChild(todoText)
+
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoText)
+        renderTodos(todos)
+    })
+
+    return todoEl
+}
+
+const createTodo = (text) => {
+    todos.push(text)
+}
 
 
 
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log("texto añadido: " + text)
+
+     if (text.length > 0) {
+        createTodo(text)
+        renderTodos(todos)
+        }
+    console.log(todos);
+})
