5c7d66ce4f62ba4f83e0530a4ad0f5342c7c2cea
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-09-20 14:57:14 +0200

    ejercicio 5 hecho

--- scripts/script.js
+++ scripts/script.js
@@ -10,10 +10,30 @@ console.log(btnCreate);*/
 
 
 4//
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
+/*document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
     const text = e.target.elements.text.value.trim()
     alert(text);
+})*/
+
+5//
+
+let todos =[]
+
+const  createTodo = (text) => {
+    todos.push(text)
+}
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log("texto añadido " + (text))
+
+    if (text.length > 0) {
+        createTodo(text)
+    }
+
+    console.log(todos);
 })
 
 
